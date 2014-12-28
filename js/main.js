ab7083f480b9b93779619deeff5214d7f405bfdd
chenzhip <letme2010@gmail.com>
2014-12-28 23:14:19 +0800

    first commit
    sound effect and background music control web app.

---
+++ js/main.js
@@ -0,0 +1,168 @@
+var gCurrentSoundIndex = 0;
+var gCurrentBgIndex = 0;
+
+function main() {
+
+    initKey();
+
+    var _jqBgMusicUl = $('#bg_music_list');
+
+    $.each(gBgMusicList, function (n, aMusic) {
+        var _jqMusicLi = $("<li>" + (n + 1) + "." + aMusic + "</li>");
+        _jqMusicLi.addClass("list_li");
+        _jqBgMusicUl.append(_jqMusicLi);
+    });
+
+    setCurrentBgIndex(0);
+
+    var _jqSoundUl = $('#sound_list');
+    $.each(gSoundList, function (n, aSound) {
+        var _jqSoundLi = $("<li>" + (n + 1) + "." + aSound + "</li>");
+        _jqSoundLi.addClass("list_li");
+        _jqSoundUl.append(_jqSoundLi);
+    });
+
+    setCurrentSoundIndex(0);
+
+}
+
+function initKey() {
+    KeyboardJS.on("o", function () {
+        onSkipToLastSound();
+    });
+
+    KeyboardJS.on('p', function () {
+        onSkipToNextSound();
+    });
+
+    KeyboardJS.on('j', function () {
+        onPlaySoundClick();
+    });
+
+    KeyboardJS.on('q', function(){
+        onSkipToLastBgMusic();
+    });
+
+    KeyboardJS.on('w', function(){
+        onSkipToNextBgMusic();
+    });
+
+    KeyboardJS.on('f', function(){
+        onPlayBgMusic();
+    });
+
+}
+
+
+function onPlayBgMusic() {
+    var audioName = gBgMusicList[gCurrentBgIndex];
+    var _jqBgPlayer = $('#bg_player');
+    _jqBgPlayer.attr('src', "./res/bg_music/" + audioName);
+    _jqBgPlayer[0].volume = gBgMusicVolume;
+    _jqBgPlayer[0].play();
+    var index = (++gCurrentBgIndex) % gBgMusicList.length;
+    setCurrentBgIndex(index);
+}
+
+function onSkipToLastBgMusic() {
+    var index;
+    if (0 == gCurrentBgIndex) {
+        index = gBgMusicList.length - 1;
+    } else {
+        index = (--gCurrentBgIndex) % gBgMusicList.length;
+    }
+    setCurrentBgIndex(index);
+}
+
+function onSkipToNextBgMusic() {
+    var index = (++gCurrentBgIndex) % gBgMusicList.length;
+    setCurrentBgIndex(index);
+}
+
+function setCurrentBgIndex(aIndex) {
+    gCurrentBgIndex = aIndex;
+    onCurrentBgIndexChanged();
+}
+
+function onCurrentBgIndexChanged() {
+    var _jqBgUl = $('#bg_music_list');
+
+    $.each(_jqBgUl.children(), function (aIndex) {
+        var _jqChild = $(_jqBgUl.children()[aIndex]);
+        if (_jqChild.hasClass('current_li')) {
+            _jqChild.removeClass('current_li');
+        }
+    });
+
+    $(_jqBgUl.children()[gCurrentBgIndex]).addClass('current_li');
+    $('#bg_music_list_scroll').scrollTop(20 * gCurrentBgIndex);
+}
+
+function setCurrentSoundIndex(aIndex) {
+    gCurrentSoundIndex = aIndex;
+    onCurrentSoundIndexChanged();
+}
+
+function onCurrentSoundIndexChanged() {
+    var _jqSoundUl = $('#sound_list');
+    $.each(_jqSoundUl.children(), function (aIndex) {
+        var _jqChild = $(_jqSoundUl.children()[aIndex]);
+        if (_jqChild.hasClass('current_li')) {
+            _jqChild.removeClass('current_li');
+        }
+    });
+
+    $(_jqSoundUl.children()[gCurrentSoundIndex]).addClass('current_li');
+    $('#sound_list_scroll').scrollTop(20 * gCurrentSoundIndex);
+}
+
+function onSkipToLastSound() {
+    var index;
+    if (0 == gCurrentSoundIndex) {
+        index = gSoundList.length - 1;
+    } else {
+        index = (--gCurrentSoundIndex) % gSoundList.length;
+    }
+    setCurrentSoundIndex(index);
+}
+
+function onSkipToNextSound() {
+    var index = (++gCurrentSoundIndex) % gSoundList.length;
+    setCurrentSoundIndex(index);
+}
+
+function onPlaySoundClick() {
+    var audioName = gSoundList[gCurrentSoundIndex];
+    var _jqSoundPlayer = $('#sound_player');
+    _jqSoundPlayer.attr('src', "./res/sound/" + audioName);
+    _jqSoundPlayer[0].play();
+    _jqSoundPlayer[0].volume = gSoundVolume;
+    var index = (++gCurrentSoundIndex) % gSoundList.length;
+    setCurrentSoundIndex(index);
+}
+
+function pathToFileName(aPath) {
+    return aPath.substring(aPath.lastIndexOf('/') + 1, aPath.length);
+}
+
+/*call back*/
+function onSoundStart() {
+    // $('#bg_music_playing').html();
+    var path = $('#sound_player').attr('src');
+    $('#sound_playing').html("Playing >> " + pathToFileName(path));
+}
+
+function onSoundEnd() {
+    $('#sound_playing').html("");    
+}
+
+function onBgMusicStart() {
+    var path = $('#bg_player').attr('src');
+    $('#bg_music_playing').html("Playing >> " + pathToFileName(path));
+}
+
+function onBgMusicEnd() {
+    $('#bg_music_playing').html("");    
+}
+
+main();
